a3214402258171611915d083f0d1e286751ebb85
Nothing <wwwaap@gmail.com>
2021-06-08 19:46:31 +0800

    Create nc68-跳台阶.js

---
+++ nc68-跳台阶.js
@@ -0,0 +1,14 @@
+function jumpFloor(number) {
+  const n = number
+  const dp = Array(n + 1).fill(0)
+  dp[1] = 1
+  dp[2] = 2
+  for(let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2]
+  }
+  
+  return dp[n]
+}
+module.exports = {
+    jumpFloor : jumpFloor
+};
