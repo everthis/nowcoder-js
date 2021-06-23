1c6c583e9dd85e3f24baa3e4411744a389858a10
Nothing <wwwaap@gmail.com>
2021-06-23 14:08:01 +0800

    Update nc140-排序.js

--- nc140-排序.js
+++ nc140-排序.js
@@ -5,6 +5,61 @@
  * @return int整型一维数组
  */
 function MySort(arr) {
+  qs(arr, 0, arr.length - 1)
+  return arr.slice()
+}
+function qs(arr, start, end) {
+  if(start >= end) return
+  const p = partition(arr, start, end)
+  qs(arr, start, p)
+  qs(arr, p + 1, end)
+}
+
+function swap(arr, i, j) {
+  const tmp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = tmp
+}
+
+function compare(a, b) {
+  return a - b
+}
+
+function partition(arr, start, end) {
+  let pivot = arr[start]
+  let s = start
+  let e = end
+  while(true) {
+    while(arr[s] < pivot) {
+      s++
+    }
+    while(pivot < arr[e]) {
+      e--
+    }
+    if(s === e) {
+      return s
+    } else if(s > e) {
+      return s - 1
+    }
+    swap(arr, s, e)
+    s++
+    e--
+  }
+}
+module.exports = {
+    MySort : MySort
+};
+
+
+// another
+
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ * 将给定数组排序
+ * @param arr int整型一维数组 待排序的数组
+ * @return int整型一维数组
+ */
+function MySort(arr) {
   mergeSort(arr, 0, arr.length - 1)
   return arr
 }
