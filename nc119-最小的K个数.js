45ec6f778b962bd3e94d203016c0bead7c61f4d5
Nothing <wwwaap@gmail.com>
2021-06-25 14:51:26 +0800

    Update nc119-最小的K个数.js

--- nc119-最小的K个数.js
+++ nc119-最小的K个数.js
@@ -81,3 +81,57 @@ class PriorityQueue {
 module.exports = {
     GetLeastNumbers_Solution : GetLeastNumbers_Solution
 };
+
+
+// another
+
+function GetLeastNumbers_Solution(input, k) {
+  const arr = []
+  if(k > input.length) return arr
+  for(let e of input) {
+    binaryInsert(arr, e, (a, b) => a - b)
+    if(arr.length > k) arr.pop()
+  }
+
+  return arr
+}
+function binaryInsert(array, insertValue, comparator = (a, b) => a - b) {
+  /*
+  * These two conditional statements are not required, but will avoid the
+  * while loop below, potentially speeding up the insert by a decent amount.
+  * */
+  if (array.length === 0 || comparator(array[0], insertValue) >= 0) {
+    array.splice(0, 0, insertValue)
+    return array;
+  } else if (array.length > 0 && comparator(array[array.length - 1], insertValue) <= 0) {
+    array.splice(array.length, 0, insertValue);
+    return array;
+  }
+  let left = 0, right = array.length;
+  let leftLast = 0, rightLast = right;
+  while (left < right) {
+    const inPos = Math.floor((right + left) / 2)
+    const compared = comparator(array[inPos], insertValue);
+    if (compared < 0) {
+      left = inPos;
+    } else if (compared > 0) {
+      right = inPos;
+    } else {
+      right = inPos;
+      left = inPos;
+    }
+    // nothing has changed, must have found limits. insert between.
+    if (leftLast === left && rightLast === right) {
+      break;
+    }
+    leftLast = left;
+    rightLast = right;
+  }
+  // use right, because Math.floor is used
+  array.splice(right, 0, insertValue);
+  return array
+}
+
+module.exports = {
+  GetLeastNumbers_Solution : GetLeastNumbers_Solution
+};
