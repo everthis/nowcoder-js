1c774a1f1e43b50c9e9315a2d8f8bcb820f25299
Nothing <wwwaap@gmail.com>
2021-07-02 17:31:53 +0800

    Update nc120-二进制中1的个数.js

--- nc120-二进制中1的个数.js
+++ nc120-二进制中1的个数.js
@@ -9,3 +9,18 @@ function NumberOf1(n) {
 module.exports = {
     NumberOf1 : NumberOf1
 };
+
+// another
+
+function NumberOf1(n) {
+  const mask = 1 << 31
+  let res = 0
+  while(n != 0) {
+    if(n & mask) res++
+    n <<= 1
+  }
+  return res
+}
+module.exports = {
+    NumberOf1 : NumberOf1
+};
