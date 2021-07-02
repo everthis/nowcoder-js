d088362111e228d709f55980881c7f2d6792e142
Nothing <wwwaap@gmail.com>
2021-07-02 17:38:51 +0800

    Update nc103-反转字符串.js

--- nc103-反转字符串.js
+++ nc103-反转字符串.js
@@ -9,3 +9,29 @@ function solve( str ) {
 module.exports = {
     solve : solve
 };
+
+// another
+
+/**
+ * 反转字符串
+ * @param str string字符串 
+ * @return string字符串
+ */
+function solve( str ) {
+  const arr = str.split('')
+  let i = 0, j = arr.length - 1
+  while(i < j) {
+    swap(arr, i, j)
+    i++
+    j--
+  }
+  return arr.join('')
+}
+function swap(arr, i, j) {
+  let tmp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = tmp
+}
+module.exports = {
+    solve : solve
+};
