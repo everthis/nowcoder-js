c174a8170a7a2b62735f324c0324bdba9d7b78d3
Nothing <wwwaap@gmail.com>
2021-07-06 10:53:25 +0800

    Update nc51-合并k个已排序的链表.js

--- nc51-合并k个已排序的链表.js
+++ nc51-合并k个已排序的链表.js
@@ -11,6 +11,48 @@
  * @return ListNode类
  */
 function mergeKLists(lists) {
+  let node = lists[0]
+  for(let i = 1, n = lists.length; i < n; i++) {
+    node = mergeTwo(node, lists[i])
+  }
+  return node
+}
+function mergeTwo(head1, head2) {
+  const dummy = new ListNode()
+  let cur = dummy
+  while(head1 && head2) {
+    if(head1.val < head2.val) {
+      cur.next = head1
+      head1 = head1.next
+    } else {
+      cur.next = head2
+      head2 = head2.next
+    }
+    cur = cur.next
+  }
+  if(head1) cur.next = head1
+  if(head2) cur.next = head2
+  return dummy.next
+}
+module.exports = {
+  mergeKLists : mergeKLists
+};
+
+// another
+
+/*
+ * function ListNode(x){
+ *   this.val = x;
+ *   this.next = null;
+ * }
+ */
+
+/**
+ * 
+ * @param lists ListNode类一维数组 
+ * @return ListNode类
+ */
+function mergeKLists(lists) {
   return merge(lists, 0, lists.length - 1)
 }
 
